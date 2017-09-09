1f3adf397f8ac6cf697dfa4e44904475d9d1f4ee
Sam Briggs <wasso14@hotmail.com>
2017-09-09 18:30:45 +0100

    Use heroku environment variables for credentials

--- server/config.ts
+++ server/config.ts
@@ -1,6 +1,9 @@
 export const serverPort = 4300;
-export const secret = "RbBQqA6uF#msRF8s7h*?@=95HUm&DgMDd6zLFn4XzWQ6dtwXSJwBX#?gL2JWf!";
+export const secret = process.env.SERVER_SECRET || '';
 export const length = 128;
 export const digest = "sha256";
-export const nationalRailCredentials = { 'username': 'wasso14@hotmail.com', 'password': 'Manunited14!' };
-export const isStubHspApi = true;
+export const nationalRailCredentials = {
+  'username': process.env.NAT_RAIL_USERNAME || '',
+  'password': process.env.NAT_RAIL_PASSWORD || ''
+};
+export const isStubHspApi = false;
