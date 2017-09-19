7bb6efec162f81422076428b3972d83a418a5929
Sam Briggs <wasso14@hotmail.com>
2017-09-19 01:50:06 +0100

    Fix up simple app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,45 @@
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+import { HspApiService } from './hsp-api.service';
+import { ResourceService } from './national_rail/resource.service';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common'
 
 describe('AppComponent', function () {
   let de: DebugElement;
   let comp: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
+  // Configure the test bed
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      declarations: [ ],
+      imports: [ AppModule ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ]
     })
     .compileComponents();
   }));
 
+  // Create the component under test
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
     de = fixture.debugElement.query(By.css('h1'));
   });
 
+  // Test component created
   it('should create component', () => expect(comp).toBeDefined() );
 
+  // Test title
   it('should have expected <h1> text', () => {
     fixture.detectChanges();
     const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
+    expect(h1.innerText).toMatch(/Late Mate/i,
+      '<h1> should say something about "Late Mate"');
   });
 });
