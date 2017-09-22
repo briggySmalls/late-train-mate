6feb41c08ca3f4311d3c52169683e3ba8ab06551
Sam Briggs <wasso14@hotmail.com>
2017-09-23 00:01:37 +0100

    Fix server lint errors

--- server/config.ts
+++ server/config.ts
@@ -1,9 +1,14 @@
+export interface INationalRailCredentials {
+  password: string;
+  username: string;
+}
+
 export const serverPort = 4300;
-export const secret = process.env.SERVER_SECRET || '';
+export const secret = process.env.SERVER_SECRET || "";
 export const length = 128;
 export const digest = "sha256";
-export const nationalRailCredentials = {
-  'username': process.env.NAT_RAIL_USERNAME || 'sjbriggs14@gmail.com',
-  'password': process.env.NAT_RAIL_PASSWORD || 'Manunited14!'
+export const nationalRailCredentials: INationalRailCredentials = {
+  password: process.env.NAT_RAIL_PASSWORD || "Manunited14!",
+  username: process.env.NAT_RAIL_USERNAME || "sjbriggs14@gmail.com",
 };
 export const isStubHspApi = false;
--- server/routes/hsp.ts
+++ server/routes/hsp.ts
@@ -1,31 +1,31 @@
-import { Router, Response, Request } from 'express';
-import { nationalRailCredentials, isStubHspApi } from '../config';
-import * as request from 'request';
-import * as fs from 'fs';
+import { Request, Response, Router } from "express";
+import * as fs from "fs";
+import * as request from "request";
+import { isStubHspApi, nationalRailCredentials } from "../config";
 
 const hspRouter: Router = Router();
 
 export enum Resource {
     ServiceMetrics,
-    ServiceDetails
+    ServiceDetails,
 }
 
-const HSP_ROOT = 'https://hsp-prod.rockshore.net/api'
-const HSP_VERSION = 'v1'
+const HSP_ROOT = "https://hsp-prod.rockshore.net/api";
+const HSP_VERSION = "v1";
 const RESOURCES = [
-    { 'resource': Resource.ServiceMetrics, 'endpoint': 'serviceMetrics'},
-    { 'resource': Resource.ServiceDetails, 'endpoint': 'serviceDetails'},
-]
+    { resource: Resource.ServiceMetrics, endpoint: "serviceMetrics"},
+    { resource: Resource.ServiceDetails, endpoint: "serviceDetails"},
+];
 
 // Debug
 const TEST_DATA_PATH = `${__dirname}/test-data`;
 
-interface ReadJsonCallback { (json: any): void };
+interface IReadJsonCallback { (json: any): void; };
 
 function hspUri(resource: Resource): string {
     let endpoint: string;
     for (let res of RESOURCES) {
-        if (res.resource == resource) {
+        if (res.resource === resource) {
             endpoint = res.endpoint;
             break;
         }
@@ -37,46 +37,46 @@ function forwardRequest(req: Request, res: Response, resource: Resource): void {
     // Make a request to HSP API
     request.post(
         hspUri(resource),
-        { 'json': req.body },
+        { json: req.body },
         (hspError, hspRes, hspBody) => {
             res.statusCode = hspRes.statusCode;
             res.json(hspBody);
         }).auth(
-            nationalRailCredentials['username'],
-            nationalRailCredentials['password'])
+            nationalRailCredentials.username,
+            nationalRailCredentials.password);
 }
 
 function fileResults(req: Request, res: Response, resource: Resource): void {
-    if (resource == Resource.ServiceDetails) {
-        readFile(`${TEST_DATA_PATH}/SD-${req.body['rid']}.json`, (json) => {
+    if (resource === Resource.ServiceDetails) {
+        readFile(`${TEST_DATA_PATH}/SD-${req.body.rid}.json`, (json) => {
             res.statusCode = 200; // TODO: See if there is an enum
             res.json(json);
-        })
-    } else if (resource == Resource.ServiceMetrics) {
+        });
+    } else if (resource === Resource.ServiceMetrics) {
         readFile(`${TEST_DATA_PATH}/SM-FPK-CBG-0000-2359-20161001-20161101-WEEKDAY-[30].json`, (json) => {
             res.statusCode = 200; // TODO: See if there is an enum
             res.json(json);
-        })
+        });
     }
 }
 
-function readFile(filename: string, callback: ReadJsonCallback): void {
-    var obj;
-    fs.readFile(filename, 'utf8', (err, data) => {
-      if (err) throw err;
+function readFile(filename: string, callback: IReadJsonCallback): void {
+    let obj;
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) { throw err; }
       callback(JSON.parse(data));
     });
 }
 
-hspRouter.post('/metrics', (req: Request, res: Response) => {
+hspRouter.post("/metrics", (req: Request, res: Response) => {
     if (!isStubHspApi) {
         forwardRequest(req, res, Resource.ServiceMetrics);
-    } else {    
+    } else {
         fileResults(req, res, Resource.ServiceMetrics);
     }
 });
 
-hspRouter.post('/details', (req: Request, res: Response) => {
+hspRouter.post("/details", (req: Request, res: Response) => {
     if (!isStubHspApi) {
         forwardRequest(req, res, Resource.ServiceDetails);
     } else {
