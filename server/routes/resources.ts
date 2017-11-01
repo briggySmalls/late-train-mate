0b78b5fe6710b326d5230b8be7888dfb908032b1
Sam Briggs <wasso14@hotmail.com>
2017-11-01 01:10:30 +0000

    Use resource file obtained from Darwin FTP
    Both cancellation reasons and stations are found in the handy xml file
    Adds api for obtaining cancellation reasons

--- server/routes/resources.ts
+++ server/routes/resources.ts
@@ -1,6 +1,7 @@
 import * as assert from "assert";
 import { Request, Response, Router } from "express";
 import * as fs from "fs";
+
 import * as request from "request";
 import { Parser } from "xml2js";
 
@@ -8,46 +9,106 @@ import { Parser } from "xml2js";
  * TODO: Remove 'I' and potentially replace with a class?
  */
 interface IStation {
-    display: string;
-    value: string;
+  code: string;
+  text: string;
+}
+
+interface IReason {
+  code: string;
+  text: string;
 }
 
 const resourcesRouter: Router = Router();
 const RESOURCES_PATH = `${__dirname}/resources`;
-const STATIONS_FILE_PATH = `${RESOURCES_PATH}/station-codes.xml`;
+const STATIONS_FILE_PATH = `${RESOURCES_PATH}/*_ref_v3.xml`;
 
 // Populate the stations list immediately
-let allStations: IStation[];
-load_stations();
+let allStations: IStation[] = [];
+let allReasons: IReason[] = [];
 
-function load_stations() {
-  // Create a new XML parser
-  let p = new Parser();
+// Load the resources from disk when we load the module
+loadResources();
 
-  // Parse the stations file
-  fs.readFile(STATIONS_FILE_PATH, "utf8", (readErr, data) => {
-    if (readErr) {
-      throw readErr;
+/**
+ * @brief      Loads resources from the resources file
+ * @return     None
+ */
+function loadResources() {
+  // First find potential resource files
+  fs.readdir(RESOURCES_PATH, (dirErr, files) => {
+    if (dirErr) {
+      throw dirErr;
     }
-    allStations = [];
-    // Parse the station-codes xml document
-    p.parseString(data, (parseErr, result) => {
-      if (parseErr) {
-        throw parseErr;
+    assert(files.length > 0, "No resource file found");
+
+    // Create a new XML parser
+    let p = new Parser();
+
+    // Read the most-recent resources file
+    fs.readFile(`${RESOURCES_PATH}/${files.sort()[0]}`, "utf8", (readErr, data) => {
+      if (readErr) {
+        throw readErr;
       }
-      // Add a key for each station
-      for (let station of result.StationList.Station) {
-        assert(station.Name.length === 1);
-        assert(station.CrsCode.length === 1);
-        allStations.push({value: station.CrsCode[0], display: station.Name[0]});
-      }
+      // Parse the xml content
+      p.parseString(data, (parseErr, result) => {
+        if (parseErr) {
+          throw parseErr;
+        }
+        loadStations(result);
+        loadReasons(result);
+      });
     });
   });
+}
+
+/**
+ * @brief      Loads stations from the resource data
+ *
+ * @param      data  The parsed xml data
+ * @return     None
+ */
+function loadStations(data: any) {
+  allStations = [];
+  // Add a key for each station
+  for (let location of data.PportTimetableRef.LocationRef) {
+    if (location.$.hasOwnProperty("toc")) {
+      assert(location.$.hasOwnProperty("crs"), `Station with TOC: ${location.$.toc} is missing crs property`);
+      assert(location.$.hasOwnProperty("locname"), `Station ${location.$.crs} is missing locname property`);
+      allStations.push({code: location.$.crs, text: location.$.locname});
+    }
+  }
+}
+
+/**
+ * @brief      Loads reasons from the resource data
+ *
+ * @param      data  The parsed xml data
+ * @return     None
+ */
+function loadReasons(data: any) {
+  allReasons = [];
+  // Add a key for each late running reason
+  for (let reason of data.PportTimetableRef.LateRunningReasons[0].Reason) {
+    assert(reason.$.hasOwnProperty("code"));
+    assert(reason.$.hasOwnProperty("reasontext"));
+    allReasons.push({code: reason.$.code, text: reason.$.reasontext});
+  }
+  // Add a key for each cancellation reason
+  for (let reason of data.PportTimetableRef.CancellationReasons[0].Reason) {
+    assert(reason.$.hasOwnProperty("code"));
+    assert(reason.$.hasOwnProperty("reasontext"));
+    allReasons.push({code: reason.$.code, text: reason.$.reasontext});
+  }
 }
 
 // Get all stations
 resourcesRouter.get("/stations", (request: Request, response: Response) => {
   response.json(allStations);
+});
+
+// Get all reasons
+resourcesRouter.get("/reasons", (request: Request, response: Response) => {
+  response.json(allReasons);
 });
 
 export { resourcesRouter };
